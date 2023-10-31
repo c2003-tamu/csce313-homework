0ddf421afe9c550c0111f8ebd867f90ec83b8f7d
Cade Royal <c2003@tamu.edu>
2023-10-31 20:10:30 +0000

    patched q3

--- q3/q3.cpp
+++ q3/q3.cpp
@@ -9,12 +9,13 @@ using std::string, std::cout, std::cin, std::endl;
 
 int main(int argc, char* argv[]){
     
-    // int p[2];
-    // int myPipe = pipe(p);
-    // if(myPipe==-1){
-    //     cout << "could not open pipe";
-    //     return 1;
-    // }
+    //bool complete = false;
+    int p[2];
+    int myPipe = pipe(p);
+    if(myPipe==-1){
+        cout << "could not open pipe";
+        return 1;
+    }
 
     int file = open(argv[1], O_RDONLY);
     if(file == -1){
@@ -30,9 +31,29 @@ int main(int argc, char* argv[]){
         cout << "could not fork" << endl;
     }else if(child == 0){
         //cout << "in child" << endl;
+        //close(myPipe[1]);
+
+        while(true){
+            bool receivedValue;
+            char receivedByte;
+            if (read(p[0], &receivedByte, sizeof(receivedByte)) == -1) {
+                perror("read");
+                return 1;
+            }
+            receivedValue = (receivedByte == 1);
+
+            if(receivedValue){
+                break;
+            }
+        }
+        cout << "entering child"<<endl;
+
         string filename = "tests/testout.txt";
         int outFile = open(filename.c_str(), O_WRONLY | O_CREAT, S_IRUSR | S_IWUSR);
 
+        /*
+        * sending data
+        */
         ssize_t bytesRead;
         while ((bytesRead = read(file, buffer, sizeof(buffer))) > 0) {
             ssize_t bytesWritten = write(outFile, buffer, bytesRead);
@@ -41,12 +62,28 @@ int main(int argc, char* argv[]){
                 return 1;
             }
         }
+
+
+
     }else{
        // cout << "in parent" << endl;
         int deleted = unlink(argv[1]);
         if(deleted == -1){
             cout << "error deleting file" <<endl;
         }
+        //close(myPipe[0]);  // Close the read end of the pipe in the parent
+
+        bool valueToSend = true;
+
+        // Convert the boolean value to a byte and write it to the pipe
+        char byteToSend = valueToSend ? 1 : 0;
+        if (write(p[1], &byteToSend, sizeof(byteToSend)) == -1) {
+            perror("write");
+            return 1;
+        }
+        cout << endl<< "Parent Complete!!!"<<endl;
+        //close(myPipe[1]);  // Close the write end of the pipe in the parent
+
 
     }
     
--- q3/testq3.sh
+++ q3/testq3.sh
@@ -1,5 +1,7 @@
 g++ *.cpp;
+rm tests/testout.txt
 ./a.out tests/test1.txt;
 cat tests/testout.txt;
+rm tests/testout.txt
 ./a.out tests/test2.txt;
 cat tests/testout.txt;
