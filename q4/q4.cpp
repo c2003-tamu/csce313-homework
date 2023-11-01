f76ef1fde085458fd592b86ad588a4e376298d4e
Cade Royal <c2003@tamu.edu>
2023-11-01 22:44:01 +0000

    patched q4

--- q4/q4.cpp
+++ q4/q4.cpp
@@ -3,9 +3,10 @@
 #include <unistd.h>
 #include <string>
 #include <sys/stat.h>
+#include <filesystem>
 
 
-using std::string, std::cout, std::cin, std::endl;
+using std::string, std::cout, std::cin, std::endl, std::filesystem::is_regular_file, std::filesystem::is_fifo, std::filesystem::is_symlink;
 
 
 int main(int argc, char* argv[]){
@@ -17,6 +18,18 @@ int main(int argc, char* argv[]){
         cout << "File permissions: " << (fileStat.st_mode & 0777) << endl;
         // Add more attributes as needed
         cout << "id of owner: " << fileStat.st_uid <<endl;
+
+        cout << "type of file: ";
+        if(is_regular_file(argv[1])){
+            cout << "Regular File" << endl;
+        }else if(is_fifo(argv[1])){
+            cout<< "pipe" << endl;
+        }else if(is_symlink(argv[1])){
+            cout << "sym link" <<endl;
+        }else{
+            cout << "unknown file type"<<endl;
+        }
+
     } else {
         std::cerr << "Error: Unable to obtain file information" << endl;
     }
